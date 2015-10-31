d3d51eec77bc6723c539ed820f55a971d0332c1a
Vašek Dohnal <vaclav.dohnal@gmail.com>
2015-10-31 21:14:51 +0100

    + celery

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.src_dir}}/{{cookiecutter.main_app}}/celery.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ cookiecutter.main_app }}.settings.base')
+
+import celery
+from django.conf import settings
+
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        if hasattr(settings, 'RAVEN_CONFIG') and settings.RAVEN_CONFIG['dsn']:
+            import raven
+            from raven.contrib.celery import (register_signal,
+                                              register_logger_signal)
+
+            client = raven.Client(settings.RAVEN_CONFIG['dsn'])
+            register_logger_signal(client)
+            register_signal(client)
+
+
+app = Celery('{{ cookiecutter.main_app }}')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
