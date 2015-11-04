39267a4cc189252cab4a2eb702d60229b5a16840
Vašek Dohnal <vaclav.dohnal@gmail.com>
2015-11-04 15:36:15 +0100

    Simplified tests settings.

--- {{cookiecutter.repo_name}}/{{cookiecutter.src_dir}}/{{cookiecutter.main_app}}/settings/test.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.src_dir}}/{{cookiecutter.main_app}}/settings/test.py
@@ -8,9 +8,6 @@ from __future__ import absolute_import
 import logging
 import os
 
-from .base import DATABASES
-
-
 DEBUG = False
 TEMPLATE_DEBUG = False
 os.environ['REUSE_DB'] = "1"
@@ -21,9 +18,11 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
 
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': ':memory:'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
 }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
