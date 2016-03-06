34b0582b0778b78544b0624fdd04e8729f2a40bd
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-03-06 11:27:57 +0100

    Fix pyreadline version.

--- {{cookiecutter.repo_name}}/activate.ps1
+++ {{cookiecutter.repo_name}}/activate.ps1
@@ -1,2 +1,4 @@
 # Activate virtual environment from PowerShell
+# Upgrade virtualenv: pip install virtualenv --upgrade
+# Create virtualenv:  virtualenv C:\Users\...\.virtualenvs\{{ cookiecutter.repo_name }}
 ~\.virtualenvs\{{ cookiecutter.repo_name }}\Scripts\activate.ps1
