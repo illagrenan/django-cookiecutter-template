3362835f0cf041e1452c192e28312db8b278fc4c
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-08-17 01:16:50 +0200

    Updated requirements; Fixed humans.txt link; Changed datetime format in logging.

--- {{cookiecutter.repo_name}}/{{cookiecutter.src_dir}}/{{cookiecutter.main_app}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.src_dir}}/{{cookiecutter.main_app}}/settings/base.py
@@ -341,8 +341,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S"
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(module)s - %(funcName)s:%(lineno)s] %(message)s",
+            'datefmt': "%d.%m.%Y %H:%M:%S"
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
