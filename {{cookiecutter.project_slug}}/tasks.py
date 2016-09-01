ab9a5a98f0689657c92088b264b474370756d25e
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-09-01 11:53:26 +0200

    Added coverage support; Changed main directory to project_slug (instead repo_name).

---
+++ {{cookiecutter.project_slug}}/tasks.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+# ! python3
+
+import logging
+
+from invoke import run, task
+
+logger = logging.getLogger(__name__)
+
+
+@task
+def worker():
+    """ Run Celery Worker """
+    run("celery worker --workdir=src/ --app=main --concurrency=2 --loglevel=INFO")
+
+
+@task
+def worker():
+    """ Run Celery Beat """
+    run("celery beat --workdir=src/ --app=main --loglevel=INFO")
+
+
+@task
+def test():
+    """ Test project """
+    run("pytest --verbose --showlocals")
+
+
+@task
+def coverage():
+    """ Create coverage report """
+    run("coverage run pytest")
+    run("coverage report -m")
+    run("coverage html")
--- {{cookiecutter.repo_name}}/activate.ps1
+++
@@ -1,3 +0,0 @@
-# Activate virtualenv from PowerShell
-# Create virtualenv: $ py -3 -m venv {{ cookiecutter.python_interpreter_dir }}..\
-{{ cookiecutter.python_interpreter_dir }}\activate.ps1
