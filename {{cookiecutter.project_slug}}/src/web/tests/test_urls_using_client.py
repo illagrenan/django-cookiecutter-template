c74e7fc1a06397902f0a28dc74a3ed5a4db1eaeb
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-10-09 16:13:39 +0200

    Added test client example.

---
+++ {{cookiecutter.project_slug}}/src/web/tests/test_urls_using_client.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+# ! python3
+
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
+
+
+class UrlsClientTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_admin_login_screen(self):
+        response = self.client.get(reverse("admin:index"), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_default_page(self):
+        response = self.client.get(reverse("web:default_page"), follow=False)
+        self.assertEqual(response.status_code, 200)
