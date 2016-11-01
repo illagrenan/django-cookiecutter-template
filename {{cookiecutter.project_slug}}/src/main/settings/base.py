b81de2285d70457208f304cd531775bad6b7f3e5
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-11-01 11:10:33 +0100

    Use bcrypt as password hashed.

--- {{cookiecutter.project_slug}}/src/main/settings/base.py
+++ {{cookiecutter.project_slug}}/src/main/settings/base.py
@@ -282,11 +282,11 @@ AUTHENTICATION_BACKENDS = [
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/es/dev/ref/settings/#std:setting-PASSWORD_HASHERS
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
 ]
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
