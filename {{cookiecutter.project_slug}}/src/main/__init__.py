2c91c7774bc9319cc6c4905c9a7ef04d3bee880c
Vašek Dohnal <vaclav.dohnal@gmail.com>
2017-04-03 15:24:28 +0200

    Added __all__ for Celery app.

--- {{cookiecutter.project_slug}}/src/main/__init__.py
+++ {{cookiecutter.project_slug}}/src/main/__init__.py
@@ -6,3 +6,5 @@ default_app_config = 'main.apps.MainAppsConfig'
 # This will make sure the app is always imported when Django starts so that shared_task will use this app.
 # noinspection PyUnresolvedReferences
 from .celery import app as celery_app  # noqa
+
+__all__ = ['celery_app']
