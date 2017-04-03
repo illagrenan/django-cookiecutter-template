ba1833b641212712856a6fe99fce52b2738e131e
Vašek Dohnal <vaclav.dohnal@gmail.com>
2017-04-03 15:25:28 +0200

    Configuration for Celery 4.x (Dropped support for Celery 3.x).

--- {{cookiecutter.project_slug}}/src/main/celery.py
+++ {{cookiecutter.project_slug}}/src/main/celery.py
@@ -2,13 +2,14 @@
 # ! python3
 
 import os
+from datetime import timedelta
 
 import celery
 from django.conf import settings
 from kombu import Exchange, Queue
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.base')
+if not settings.configured:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.base')
 
 
 class Celery(celery.Celery):
@@ -25,24 +26,39 @@ class Celery(celery.Celery):
 app = Celery('{prefix}_celery_app'.format(prefix=settings.REDIS_PREFIX))
 app.config_from_object('django.conf:settings')
 app.conf.update(
-    CELERY_TASK_SERIALIZER='json',
-    CELERY_IGNORE_RESULT=True,
-    CELERY_RESULT_SERIALIZER='json',
-    CELERY_ACCEPT_CONTENT=['json'],
-    CELERY_ENABLE_UTC=True,
-    CELERY_TIMEZONE=settings.TIME_ZONE,
-    BROKER_URL=settings.REDIS_URL,
-    BROKER_TRANSPORT_OPTIONS={
+    task_serializer='json',
+    task_ignore_result=True,
+    result_serializer='json',
+    accept_content=['json'],
+    enable_utc=True,
+    timezone=settings.TIME_ZONE,
+    broker_url=settings.REDIS_URL,
+    task_always_eager=settings.CELERY_TASK_ALWAYS_EAGER,
+    broker_transport_options={
         'fanout_prefix': True,
         'fanout_patterns': True
     },
-    CELERY_DEFAULT_QUEUE='{prefix}_default'.format(prefix=settings.REDIS_PREFIX),
-    CELERY_DEFAULT_EXCHANGE='{prefix}_exchange'.format(prefix=settings.REDIS_PREFIX),
-    CELERY_DEFAULT_ROUTING_KEY='{prefix}_routing_key'.format(prefix=settings.REDIS_PREFIX),
-    CELERY_QUEUES=(
+    task_default_queue='{prefix}_default'.format(prefix=settings.REDIS_PREFIX),
+    task_default_exchange='{prefix}_exchange'.format(prefix=settings.REDIS_PREFIX),
+    task_default_routing_key='{prefix}_routing_key'.format(prefix=settings.REDIS_PREFIX),
+    task_queues=(
         Queue(name='{prefix}_default'.format(prefix=settings.REDIS_PREFIX),
               exchange=Exchange('{prefix}_exchange'.format(prefix=settings.REDIS_PREFIX)),
               routing_key='{prefix}_routing_key'.format(prefix=settings.REDIS_PREFIX)),
-    )
+    ),
+    beat_schedule={
+        'web:import_queries': {
+            'task': 'web:import_queries',
+            'schedule': timedelta(hours=5),
+            'args': (),
+            'kwargs': {'create_async_subtasks': True}
+        },
+        'web:import_products': {
+            'task': 'web:import_products',
+            'schedule': timedelta(hours=5),
+            'args': (),
+            'kwargs': {}
+        }
+    }
 )
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
