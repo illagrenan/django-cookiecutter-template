d85d245973eff9bf65a29c18f154e41e707b5c20
Vašek Dohnal <vaclav.dohnal@gmail.com>
2017-04-05 00:22:27 +0200

    User managers moved to separate file.

--- {{cookiecutter.project_slug}}/src/users/models.py
+++ {{cookiecutter.project_slug}}/src/users/models.py
@@ -1,36 +1,11 @@
 # -*- encoding: utf-8 -*-
 # ! python3
 
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
-
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        now = timezone.now()
-        email = self.normalize_email(email)
-        user = self.model(email=email,
-                          is_staff=is_staff, is_active=True,
-                          is_superuser=is_superuser,
-                          date_joined=now, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_user(self, email=None, password=None, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, True, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
