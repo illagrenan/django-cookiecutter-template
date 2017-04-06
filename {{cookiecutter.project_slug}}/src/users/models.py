d65f6a956da12e1f4976a905cbb37083f74c8d86
Vašek Dohnal <vaclav.dohnal@gmail.com>
2017-04-06 12:58:32 +0200

    Fixed UserManager.

--- {{cookiecutter.project_slug}}/src/users/models.py
+++ {{cookiecutter.project_slug}}/src/users/models.py
@@ -2,10 +2,12 @@
 # ! python3
 
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _lazy
+
+from users.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
